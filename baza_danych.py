dd2a9727cbfa9c7e6bd88e4cc83283fbd8fe05da
Lukasz Rutkowski <lukasz-rutkowski@users.noreply.github.com>
2018-01-02 21:44:31 +0100

    initial commit

---
+++ baza_danych.py
@@ -0,0 +1,44 @@
+from flask import render_template, flash, request
+import sqlite3
+
+baza = sqlite3.connect('baza.db', check_same_thread=False)
+
+
+def wyswietl_dane():
+	tablica = []
+	with baza as connection:
+		c = connection.cursor()
+		c.execute("""SELECT * FROM todo ORDER BY date""")
+		rows = c.fetchall()
+		for value in rows:
+			tablica.append({
+				'id': value[0],
+				'title': value[1],
+				'desc': value[2],
+				'date': value[3]
+			})
+		return tablica
+
+
+def pobierz_dane():
+	error = None
+	try:
+		if request.method == "POST":
+			tytul = request.form['title']
+			opis = request.form['desc']
+			data = request.form['date']
+			with baza as connection:
+				c = connection.cursor()
+			c.execute('INSERT INTO todo(title, desc, date) VALUES(?, ?, ?)', (tytul, opis, data))
+
+			if tytul == "":
+				flash("Please add title.")
+
+			return render_template('dodaj.html')
+
+		else:
+			return render_template('main.html')
+
+	except Exception as e:
+		flash(e)
+		return render_template('dodaj.html', error=error)
---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, flash
+import sqlite3
+from baza_danych import wyswietl_dane, pobierz_dane
+
+todo = Flask(__name__)
+todo.secret_key = "super secret key"
+
+
+@todo.route('/')
+def main():
+	zadania = wyswietl_dane()
+	return render_template('main.html', zadania=zadania)
+
+
+@todo.route('/dodaj/', methods=["GET", "POST"])
+def dodaj():
+	pobierz_dane()
+	return render_template('dodaj.html')
+
+
+@todo.route('/usun/<string:nr>', methods=["GET", "POST"])
+def usun(nr):
+	zadania = wyswietl_dane()
+	baza = sqlite3.connect('baza.db', check_same_thread=False)
+	with baza as connection:
+		c = connection.cursor()
+		polecenie = "DELETE FROM todo WHERE id={}".format(nr)
+		c.execute(polecenie)
+	flash("task deleted.")
+	return render_template('/main.html', nr=int(nr), zadania=zadania)
+
+
+if __name__ == "__main__":
+
+	todo.debug = True
+	todo.run()
