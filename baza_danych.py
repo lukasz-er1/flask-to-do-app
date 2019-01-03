d8815f98c7afa802db0fad168486c4af5ed627ff
lrutkows <lukasz.rutkowski@nokia.com>
2019-01-03 10:34:36 +0100

    fix F1 and X2

--- baza_danych.py
+++ baza_danych.py
@@ -5,40 +5,38 @@ baza = sqlite3.connect('baza.db', check_same_thread=False)
 
 
 def wyswietl_dane():
-	tablica = []
-	with baza as connection:
-		c = connection.cursor()
-		c.execute("""SELECT * FROM todo ORDER BY date""")
-		rows = c.fetchall()
-		for value in rows:
-			tablica.append({
-				'id': value[0],
-				'title': value[1],
-				'desc': value[2],
-				'date': value[3]
-			})
-		return tablica
+    tablica = []
+    with baza as connection:
+        c = connection.cursor()
+        c.execute("""SELECT * FROM todo ORDER BY date""")
+        rows = c.fetchall()
+        for value in rows:
+            tablica.append({
+                'id': value[0],
+                'title': value[1],
+                'desc': value[2],
+                'date': value[3]
+            })
+        return tablica
 
 
 def pobierz_dane():
-	error = None
-	try:
-		if request.method == "POST":
-			tytul = request.form['title']
-			opis = request.form['desc']
-			data = request.form['date']
-			with baza as connection:
-				c = connection.cursor()
-			if tytul == "":
-				flash("Please add title.")
-			else:
-				c.execute('INSERT INTO todo(title, desc, date) VALUES(?, ?, ?)', (tytul, opis, data))
+    error = None
+    try:
+        if request.method == "POST":
+            tytul = request.form['title']
+            opis = request.form['desc']
+            data = request.form['date']
+            with baza as connection:
+                c = connection.cursor()
+            if tytul == "":
+                flash("Please add title.")
+            else:
+                c.execute('INSERT INTO todo(title, desc, date) VALUES(?, ?, ?)', (tytul, opis, data))
+            return render_template('dodaj.html')
+        else:
+            return render_template('main.html')
 
-			return render_template('dodaj.html')
-
-		else:
-			return render_template('main.html')
-
-	except Exception as e:
-		flash(e)
-		return render_template('dodaj.html', error=error)
+    except Exception as e:
+        flash(e)
+        return render_template('dodaj.html', error=error)
--- main.py
+++ main.py
@@ -8,29 +8,28 @@ todo.secret_key = "super secret key"
 
 @todo.route('/')
 def main():
-	zadania = wyswietl_dane()
-	return render_template('main.html', zadania=zadania)
+    zadania = wyswietl_dane()
+    return render_template('main.html', zadania=zadania)
 
 
 @todo.route('/dodaj/', methods=["GET", "POST"])
 def dodaj():
-	pobierz_dane()
-	return render_template('dodaj.html')
+    pobierz_dane()
+    return render_template('dodaj.html')
 
 
 @todo.route('/usun/<string:nr>', methods=["GET", "POST"])
 def usun(nr):
-	baza = sqlite3.connect('baza.db', check_same_thread=False)
-	with baza as connection:
-		c = connection.cursor()
-		polecenie = "DELETE FROM todo WHERE id={}".format(nr)
-		c.execute(polecenie)
-	flash("task deleted.")
-	zadania = wyswietl_dane()
-	return render_template('/main.html', nr=int(nr), zadania=zadania)
+    baza = sqlite3.connect('baza.db', check_same_thread=False)
+    with baza as connection:
+        c = connection.cursor()
+        polecenie = "DELETE FROM todo WHERE id={}".format(nr)
+        c.execute(polecenie)
+    flash("task deleted.")
+    zadania = wyswietl_dane()
+    return render_template('/main.html', nr=int(nr), zadania=zadania)
 
 
 if __name__ == "__main__":
-
-	todo.debug = True
-	todo.run()
+    todo.debug = True
+    todo.run()
